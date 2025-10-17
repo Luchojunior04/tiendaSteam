96d477886341406ec3512a51748569240b7ab735
Ernesto56734856 <provisionalernesto@gmail.com>
2025-10-17 00:51:33 -0500

    actualizacionTiendaSteam

--- index.js
+++ index.js
@@ -19,27 +19,57 @@ const tiendaSteam = [
     
 ]
 
+
+let carrito = [] 
+
 const tiendaContenedor = document.getElementById("compra")
 
 for(let i = 0; i < tiendaSteam.length; i++){
 
     const itemContenedor = document.createElement("div")
     tiendaContenedor.appendChild(itemContenedor)
-
-
+    itemContenedor.className = 'padre'
+    
+    const itemInformacion = document.createElement('div')
+    itemContenedor.appendChild(itemInformacion)
+    itemInformacion.className = 'informacion'
+  
     const itemNombre = document.createElement("span")
-    itemContenedor.appendChild(itemNombre)
+    itemInformacion.appendChild(itemNombre)
     itemNombre.textContent = tiendaSteam[i].nombre
 
     const itemPrecio = document.createElement("span")
-    itemContenedor.appendChild(itemPrecio)
+    itemInformacion.appendChild(itemPrecio)
+    itemPrecio.className = 'precio'
     itemPrecio.textContent = tiendaSteam[i].precio + "$"
 
     const itemBoton = document.createElement("button")
     itemContenedor.appendChild(itemBoton)
+    itemBoton.className = "boton"
 
     const itemImagen = document.createElement("img")
     itemBoton.appendChild(itemImagen)
     itemImagen.src = "./agregar.png"
     itemImagen.alt = "agregar"
+
+
+    itemBoton.addEventListener('click', () =>{
+     const existe = carrito.find((elemento)=>{
+       return elemento.nombre == tiendaSteam[i].nombre
+     })
+
+     if(existe !== undefined  ){
+        //aqui tambien se puede decir solo (existe)
+        existe.cantidad = existe.cantidad + 1 //otras maneras de escribirlo existe.cantidad += 1 o existe,cantidad++
+     }else {
+         carrito.push({
+             nombre : tiendaSteam[i].nombre,
+             precio : tiendaSteam[i].precio,
+             cantidad : 1,
+
+            })
+     }
+
+        console.log(carrito)
+    })
 }
